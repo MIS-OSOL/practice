c6c5cc4fd1502b6de097c6fcb95b7f5ed8abad32
AKifumi Nakamura <nakamura.akifumi@miraitsystems.jp>
2013-07-31 14:13:12 +0900

    クラス版

---
+++ diamond_nakamura_akifumi_class.php
@@ -0,0 +1,35 @@
+<?
+class Diamond {
+  public $accessible_width = 0;
+  private $width = 0;
+
+  function __construct($w) {
+    $this->width = $w;
+  }
+
+  public funciton display() {
+    for ($i = 0; $i < $this->width; $i++) {
+      print_diamond_row($this->width, $i);
+    }
+    $j = $i;
+    for ($i = $j; $i >= 0; $i--) {
+      print_diamond_row($this->width, $i);
+    }
+  }
+
+  private function print_diamond_row($w, $i) {
+    print str_repeat(" ", $w - $i);
+    print "*";
+    if ($i != 0) {
+      print str_repeat(" ", ($i*2-1));
+      print "*";
+    }
+    print "\n";
+  }
+} 
+
+// main
+$diamond = new Diamond(5);
+$diamond.display();
+
+?>
